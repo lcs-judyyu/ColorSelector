13cde4b4e2c458de4e65ed3778add7bc20e0257c
Judy Yu <judy.yuqiuyi@gmail.com>
2022-10-27 11:15:08 -0400

    Initial Commit

---
+++ ColorSelector/ColorSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSelectorApp.swift
+//  ColorSelector
+//
+//  Created by Judy YU on 2022-10-27.
+//
+
+import SwiftUI
+
+@main
+struct ColorSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
