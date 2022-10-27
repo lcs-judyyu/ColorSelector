e3ceb3db6fcfa36b752a7e801989f70383c44a55
Judy Yu <judy.yuqiuyi@gmail.com>
2022-10-27 11:34:34 -0400

    Made the slider active by adding a property that has a @state var wrapper.

--- ColorSelector/ContentView.swift
+++ ColorSelector/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored Properties
+    @State private var selectedHue = 0.0
+    
+    
+    //MARK: Computed Properties
+    
+    //Interface
     var body: some View {
         VStack {
             VStack {
@@ -19,9 +27,9 @@ struct ContentView: View {
             Text("Hue")
                 .bold()
             
-            Text("210.7°")
+            Text("\(selectedHue)°")
             
-            Slider(value: Binding.constant(210.7),
+            Slider(value: $selectedHue,
                    in: 0...360,
                    label: { Text("Hue") },
                    minimumValueLabel: { Text("0") },
